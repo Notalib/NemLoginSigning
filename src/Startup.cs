a008161a759251b22fdb27443ff85d572b1df260
Henrik Otte Sørensen <hsn@nota.dk>
2023-04-21 15:15:53 +0200

    refactor: Remove all MVC stuff in startup.cs and add swagger

--- src/Startup.cs
+++ src/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Reflection;
 using System.Text;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Localization;
@@ -11,11 +12,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
 using NemLoginSigningService.Services;
 using NemLoginSignatureValidationService.Service;
 using NemLoginSigningWebApp.Logic;
 using NemLoginSigningCore.Logging;
 using NemLoginSigningCore.Configuration;
+
+using NemLoginSigningWebApp.Utils;
+using Serilog;
 
 namespace NemLoginSigningWebApp
 {
@@ -31,7 +36,15 @@ namespace NemLoginSigningWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllers();
+
+            // Implementation needs a httpcontext accessor, Add method does a try add.
+            services.AddHttpContextAccessor();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddScoped<ICorrelationIdAccessor, CorrelationIdAccessor>();
 
             // Configuration dependencies
             var configurationSection = Configuration.GetSection("NemloginConfiguration");
@@ -51,8 +64,6 @@ namespace NemLoginSigningWebApp
             services.AddHttpClient("ValidationServiceClient", c => c.BaseAddress = new System.Uri(nemloginConfiguration.ValidationServiceURL));
             services.AddTransient<ISigningValidationService, SigningValidationService>();
 
-            services.AddMvc();
-
             LoadAssemblies();
         }
 
@@ -62,31 +73,24 @@ namespace NemLoginSigningWebApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
 
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseSerilogRequestLogging();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            loggerFactory.AddSerilog();
+
+            LoggerCreator.LoggerFactory = loggerFactory;
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            LoggerCreator.LoggerFactory = loggerFactory;
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
 
