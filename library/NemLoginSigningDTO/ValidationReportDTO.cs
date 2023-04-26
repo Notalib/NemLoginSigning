d11929d33e6e59c3c26b6304420c26fe1b5445ee
Henrik Otte Sørensen <hsn@nota.dk>
2023-04-26 14:48:13 +0200

    refactor: Extract DTOs to seperate nugetable package

---
+++ library/NemLoginSigningDTO/ValidationReportDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace NemLoginSigningDTO
+{
+    public class ValidationReportDTO
+    {
+        public ValidationResultDTO Result { get; set; }
+
+        public string EtsiReport { get; set; }
+    }
+}
---
+++ library/NemLoginSigningDTO/ValidationResultDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NemLoginSigningDTO
+{
+    public class ValidationResultDTO
+    {
+        public string DocumentName { get; set; }
+
+        public int SignaturesCount { get; set; }
+
+        public int ValidSignaturesCount { get; set; }
+
+        public ICollection<ValidationSignatureDTO> Signatures { get; set; }
+    }
+}
